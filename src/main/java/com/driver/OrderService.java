e33155a5bae84988e00ce6090a462d2904d08281
Parteekpal Singh <sparteek9@gmail.com>
2023-05-13 15:37:32 +0530

    commit 4

--- src/main/java/com/driver/OrderService.java
+++ src/main/java/com/driver/OrderService.java
@@ -1,14 +1,17 @@
 package com.driver;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.Objects;
 
+@Service
 public class OrderService {
 
-    OrderRepository orderRepository=new OrderRepository();
+    @Autowired
+    OrderRepository orderRepository;
 
     public OrderService() {
     }
